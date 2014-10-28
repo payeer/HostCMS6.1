2fe8959829e9ad550934f1a2b29a8eedf1b9e679
dldemoks <demokstar@gmail.com>
2014-10-28 17:58:58 +0600

    update

--- payeer.php
+++ payeer.php
@@ -29,7 +29,7 @@ class Shop_Payment_System_HandlerXX extends Shop_Payment_System_Handler
 	
 	// 1 - log /payeer/orders.log ; 0 - do not write
 	
-	protected $payeer_log = 1; 
+	protected $payeer_log = 0; 
 
 	////////////////////////  End settings Payeer  //////////////////////////////
 	
@@ -92,87 +92,25 @@ class Shop_Payment_System_HandlerXX extends Shop_Payment_System_Handler
 			$m_key
 		);
 		$sign = strtoupper(hash('sha256', implode(":", $arHash)));
-		
-		$log_text = 
-			"--------------------------------------------------------\n".
-			"shop				" . $m_shop . "\n".
-			"order id			" . $m_orderid . "\n".
-			"amount				" . $m_amount . "\n".
-			"currency			" . $m_curr . "\n".
-			"sign				" . $sign . "\n\n";
-		
-		// проверка принадлежности ip списку доверенных ip
-		$list_ip_str = str_replace(' ', '', $this->ipfilter);
-		
-		if (!empty($list_ip_str)) 
-		{
-			$list_ip = explode(',', $list_ip_str);
-			$this_ip = $_SERVER['REMOTE_ADDR'];
-			$this_ip_field = explode('.', $this_ip);
-			$list_ip_field = array();
-			$i = 0;
-			$valid_ip = FALSE;
-			foreach ($list_ip as $ip)
-			{
-				$ip_field[$i] = explode('.', $ip);
-				if ((($this_ip_field[0] ==  $ip_field[$i][0]) or ($ip_field[$i][0] == '*')) and
-					(($this_ip_field[1] ==  $ip_field[$i][1]) or ($ip_field[$i][1] == '*')) and
-					(($this_ip_field[2] ==  $ip_field[$i][2]) or ($ip_field[$i][2] == '*')) and
-					(($this_ip_field[3] ==  $ip_field[$i][3]) or ($ip_field[$i][3] == '*')))
-					{
-						$valid_ip = TRUE;
-						break;
-					}
-				$i++;
-			}
-		}
-		else
-		{
-			$valid_ip = TRUE;
-		}
-		
+
         ob_start();
-		
-		if ($valid_ip)
-		{
-			?>
 
-			<h1>Payment through the system Payeer</h1>
-			<p>The amount to be paid is <strong><?php echo $m_amount?> <?php echo $currency_name?></strong></p>
-			<form action="<?=$m_url?>" name="pay" method="get">
-				<input type="hidden" name="m_shop" value="<?=$m_shop?>">
-				<input type="hidden" name="m_orderid" value="<?=$m_orderid?>">
-				<input type="hidden" name="m_amount" value="<?=$m_amount?>">
-				<input type="hidden" name="m_curr" value="<?=$m_curr?>">
-				<input type="hidden" name="m_desc" value="<?=$m_desc?>">
-				<input type="hidden" name="m_sign" value="<?=$sign?>">
-				<p><img src="https://payeer.com/images/img/logo.png" width="88" height="31" alt="The system of electronic payments Payeer" title="The system of electronic payments Payeer" style="float:left; margin-right:0.5em; margin-bottom:0.5em; padding-top:0.25em;"><b>Welcome to the website of the payment system <a href="https://payeer.com">Payeer</a></b>.</p>
-				<p><input type="submit" name="submit" value="To pay using Payeer"></p>
-			</form>
+		?>
 
-			<?php
-		}
-		else
-		{
-			$oSite_Alias = $this->_shopOrder->Shop->Site->getCurrentAlias();
-			$site_alias = !is_null($oSite_Alias) ? $oSite_Alias->name : '';
-			$to = $this->emailerror;
-			$subject = "Error payment";
-			$message = "Failed to make the payment through the system Payeer for the following reasons:\n\n";
-			$message.=" - the ip address of the server is not trusted\n";
-			$message.="   trusted ip: " . $this->ipfilter . "\n";
-			$message.="   ip of the current server: " . $_SERVER['REMOTE_ADDR'] . "\n";
-			$message.="\n" . $log_text;
-			$headers = "From: no-reply@" . $site_alias . "\r\nContent-type: text/plain; charset=utf-8 \r\n";
-			mail($to, $subject, $message, $headers);
-	
-			?>
-			
-			<h1>Payment through the system Payeer</h1>
-			<p>Your order is cancelled, the information is sent to support</p>
-			
-			<?php
-		}
+		<h1>Payment through the system Payeer</h1>
+		<p>The amount to be paid is <strong><?php echo $m_amount?> <?php echo $currency_name?></strong></p>
+		<form action="<?=$m_url?>" name="pay" method="get">
+			<input type="hidden" name="m_shop" value="<?=$m_shop?>">
+			<input type="hidden" name="m_orderid" value="<?=$m_orderid?>">
+			<input type="hidden" name="m_amount" value="<?=$m_amount?>">
+			<input type="hidden" name="m_curr" value="<?=$m_curr?>">
+			<input type="hidden" name="m_desc" value="<?=$m_desc?>">
+			<input type="hidden" name="m_sign" value="<?=$sign?>">
+			<p><img src="https://payeer.com/bitrix/templates/difiz/images/logo.png" width="88" height="31" alt="The system of electronic payments Payeer" title="The system of electronic payments Payeer" style="float:left; margin-right:0.5em; margin-bottom:0.5em; padding-top:0.25em;"><b>Welcome to the website of the payment system <a href="https://payeer.com">Payeer</a></b>.</p>
+			<p><input type="submit" name="submit" value="To pay using Payeer"></p>
+		</form>
+
+		<?php
 
         return ob_get_clean();
     }
@@ -205,6 +143,36 @@ class Shop_Payment_System_HandlerXX extends Shop_Payment_System_Handler
 				
 			$sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
 			
+			// проверка принадлежности ip списку доверенных ip
+			$list_ip_str = str_replace(' ', '', $this->ipfilter);
+			
+			if (!empty($list_ip_str)) 
+			{
+				$list_ip = explode(',', $list_ip_str);
+				$this_ip = $_SERVER['REMOTE_ADDR'];
+				$this_ip_field = explode('.', $this_ip);
+				$list_ip_field = array();
+				$i = 0;
+				$valid_ip = FALSE;
+				foreach ($list_ip as $ip)
+				{
+					$ip_field[$i] = explode('.', $ip);
+					if ((($this_ip_field[0] ==  $ip_field[$i][0]) or ($ip_field[$i][0] == '*')) and
+						(($this_ip_field[1] ==  $ip_field[$i][1]) or ($ip_field[$i][1] == '*')) and
+						(($this_ip_field[2] ==  $ip_field[$i][2]) or ($ip_field[$i][2] == '*')) and
+						(($this_ip_field[3] ==  $ip_field[$i][3]) or ($ip_field[$i][3] == '*')))
+						{
+							$valid_ip = TRUE;
+							break;
+						}
+					$i++;
+				}
+			}
+			else
+			{
+				$valid_ip = TRUE;
+			}
+		
 			$log_text = 
 				"--------------------------------------------------------\n".
 				"operation id		".$_POST["m_operation_id"]."\n".
@@ -224,7 +192,7 @@ class Shop_Payment_System_HandlerXX extends Shop_Payment_System_Handler
 				file_put_contents($_SERVER['DOCUMENT_ROOT'].'/payeer/orders.log', $log_text, FILE_APPEND);
 			}
 
-			if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success')
+			if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success' && $valid_ip)
 			{
 				$this->_shopOrder->paid();
 				$this->setXSLs();
@@ -239,14 +207,24 @@ class Shop_Payment_System_HandlerXX extends Shop_Payment_System_Handler
 				$to = $this->emailerror;
 				$subject = "Error payment";
 				$message = "Failed to make the payment through the system Payeer for the following reasons:\n\n";
+				
 				if ($_POST["m_sign"] != $sign_hash)
 				{
 					$message.=" - Do not match the digital signature\n";
 				}
+				
 				if ($_POST['m_status'] != "success")
 				{
 					$message.=" - The payment status is not success\n";
 				}
+				
+				if (!$valid_ip)
+				{
+					$message.=" - the ip address of the server is not trusted\n";
+					$message.="   trusted ip: " . $this->ipfilter . "\n";
+					$message.="   ip of the current server: " . $_SERVER['REMOTE_ADDR'] . "\n";
+				}
+				
 				$message.="\n" . $log_text;
 				$headers = "From: no-reply@" . $site_alias . "\r\nContent-type: text/plain; charset=utf-8 \r\n";
 				mail($to, $subject, $message, $headers);
